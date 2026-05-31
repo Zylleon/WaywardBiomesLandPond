11e5be3199e40dd55f80ea172633281f37d83f06
Zylleon <56230821+Zylleon@users.noreply.github.com>
2026-05-31 12:04:33 -0600

    biomeworker and world map texture

---
+++ Source/ZWB_LotusWilds/BiomeWorker_Test.cs
@@ -0,0 +1,44 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld.Planet;
+using Verse;
+
+namespace ZWB_LotusWilds
+{
+    public class BiomeWorker_LotusWilds : BiomeWorker
+    {
+
+        public override float GetScore(BiomeDef biome, Tile tile, PlanetTile planetTile)
+        {
+            if (tile.WaterCovered)
+            {
+                return -100f;
+            }
+
+            if(tile.rainfall < 1400f || tile.rainfall > 2600f)
+            {
+                return 0;
+            }
+            if(tile.temperature < -3f || tile.temperature > 10f)
+            {
+                return 0;
+            }
+            if(tile.swampiness > 0.5f)
+            {
+                return 0f;
+            }
+            if (tile.elevation > 450f)
+            {
+                return 0f;
+            }
+
+            return 100f;
+        }
+    }
+
+
+}
